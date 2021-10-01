a767534b306f169fccc0c31e9df30c480866d9e1
Fredrik Teschke <f@ftes.de>
2021-10-01 23:43:15 +0200

    ✨ Integrate tailwind with phx hook

--- lib/petals_drag_n_drop_web/live/page_live.ex
+++ lib/petals_drag_n_drop_web/live/page_live.ex
@@ -6,7 +6,20 @@ defmodule PetalsDragNDropWeb.PageLive do
   @impl true
   def render(assigns) do
     ~F"""
-      <div>Hello {@world}</div>
+      <div
+        id="page"
+        :hook="Page"
+        x-data
+        x-init="setTimeout(() => { pageHook.pushEvent('set_world', 'WORLD!') }, 1000)"
+        class="h-screen flex justify-center items-center font-bold text-3xl"
+      >
+        Hello {@world}
+      </div>
     """
+  end
+
+  @impl true
+  def handle_event("set_world", value, socket) do
+    {:noreply, assign(socket, world: value)}
   end
 end
