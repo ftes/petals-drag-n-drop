a70ae05a72b4d97cd40b94245dab4ad0e51b3ef1
Fredrik Teschke <f@ftes.de>
2021-10-01 23:33:13 +0200

    ✨ Add alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,7 @@
 //     import "some-package"
 //
 
+import Alpine from "alpinejs"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
@@ -27,8 +28,18 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import hooks from "./_hooks"
 
+window.Alpine = Alpine;
+Alpine.start();
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
+    dom: {
+        onBeforeElUpdated(from, to) {
+            if (from._x_dataStack) {
+                window.Alpine.clone(from, to)
+            }
+        },
+    },
     hooks,
     params: {_csrf_token: csrfToken},
 })
